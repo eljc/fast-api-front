08b24f8d8133b1db3213885852c0eb2918b835a6
Elderjc <elder.jc@hotmail.com>
2023-08-09 07:53:53 -0300

    adjuste layou

--- src/Posts.js
+++ src/Posts.js
@@ -1,6 +1,16 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { fetchToken } from "./Auth";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
 
 const hdrs = {
   "Content-type": "application/json",
@@ -17,7 +27,6 @@ export default function Posts() {
           headers: hdrs,
         });
         setPosts(response.data.posts);
-        console.log(response.data.posts);
       } catch (error) {
         console.log(error);
       }
@@ -26,16 +35,42 @@ export default function Posts() {
   }, []);
 
   return (
-    <div className="posts-container">
-      <h2>All Posts</h2>
-      {posts.map((post) => {
-        return (
-          <div className="post-card" key={post.title}>
-            <h2 className="post-title">{post.content}</h2>
-            <p className="post-body">{post.category}</p>
-          </div>
-        );
-      })}
-    </div>
+    <main>
+      <Container sx={{ py: 8 }} maxWidth="md">
+        <Grid container spacing={4}>
+          {posts.map((post) => (
+            <Grid item key={post} xs={12} sm={6} md={4}>
+              <Card
+                sx={{
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <CardMedia
+                  component="div"
+                  sx={{
+                    // 16:9
+                    pt: "56.25%",
+                  }}
+                  image={post.image}
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {post.title}
+                  </Typography>
+                  <Typography>{post.content}</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">View</Button>
+                  <Button size="small">Edit</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      ;
+    </main>
   );
 }
